4d6c1288d0ed5b30187ee7fe675eb462f5bc37d1
Oscar <odmarulandab@unal.edu.co>
2025-09-13 10:54:48 -0500

    add tailwind and add styles to the characterCard and home

--- rickAndMorty/src/components/CharacterCard.tsx
+++ rickAndMorty/src/components/CharacterCard.tsx
@@ -6,10 +6,18 @@ interface Props {
 
 export default function CharacterCard({ character }: Props) {
   return (
-    <div className="card">
-      <img src={character.image} alt={character.name} width={200} />
-      <h2>{character.name}</h2>
-      <p>{character.status} - {character.species}</p>
+    <div className="bg-gray-50 dark:bg-gray-800 rounded-lg shadow hover:shadow-lg transition p-5 flex flex-col items-center text-center">
+      <img
+        src={character.image}
+        alt={character.name}
+        className="w-48 h-48 rounded-full mb-4"
+      />
+      <h2 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {character.name}
+      </h2>
+      <p className="text-gray-500 dark:text-gray-400 mt-2">
+        {character.status} - {character.species}
+      </p>
     </div>
   );
 }
